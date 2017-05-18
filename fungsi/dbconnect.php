d8d196a43e74eb066db5a96aa3a4863bf6026703
Andika Kamiswara <andikatfb2@gmail.com>
2017-05-18 12:24:07 +0700

    conneksi di rubah jika menggunakan php 5
    menggunakan mysqli

--- fungsi/dbconnect.php
+++ fungsi/dbconnect.php
@@ -1,21 +1,28 @@
 <?php
 
-	// this will avoid mysql_connect() deprecation error.
-	error_reporting( ~E_DEPRECATED & ~E_NOTICE );
-	// but I strongly suggest you to use PDO or MySQLi.
-	
-	define('DBHOST', 'localhost');
-	define('DBUSER', 'root');
-	define('DBPASS', '');
-	define('DBNAME', 'db_pmb');
-	
-	$conn = mysql_connect(DBHOST,DBUSER,DBPASS);
-	$dbcon = mysql_select_db(DBNAME);
-	
-	if ( !$conn ) {
-		die("Connection failed : " . mysql_error());
-	}
-	
-	if ( !$dbcon ) {
-		die("Database Connection failed : " . mysql_error());
-	}
+//	// koneksi yang lama
+//	error_reporting( ~E_DEPRECATED & ~E_NOTICE );
+//	// but I strongly suggest you to use PDO or MySQLi.
+//
+//	define('DBHOST', 'localhost');
+//	define('DBUSER', 'root');
+//	define('DBPASS', '');
+//	define('DBNAME', 'db_pmb');
+//
+//	$conn = mysql_connect(DBHOST,DBUSER,DBPASS);
+//	$dbcon = mysql_select_db(DBNAME);
+//	if ( !$conn ) {
+//		die("Connection failed : " . mysql_error());
+//	}
+//
+//	if ( !$dbcon ) {
+//		die("Database Connection failed : " . mysql_error());
+//	}
+//	koneksi mysqli
+$con = mysqli_connect("localhost","root","","db_pmb");
+
+// Check connection
+if (mysqli_connect_errno())
+{
+    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
